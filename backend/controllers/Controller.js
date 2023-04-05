562536b9f6810ab35e9c8af85011819a378c2c55
unknown <liza.2002.norko@gmail.com>
2023-04-05 20:21:13 +0200

    Delete Incomes and Expenses

--- backend/controllers/Controller.js
+++ backend/controllers/Controller.js
@@ -75,6 +75,30 @@ exports.getIncomes = async (req, res) =>{
     }
 }
 
+exports.deleteExpense = async (req, res) =>{
+    const {id} = req.params;
+    ExpenseSchema.findByIdAndDelete(id)
+        .then((income) =>{
+            res.status(200).json({message: 'Expense Deleted'})
+        })
+        .catch((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
+}
+
+exports.deleteIncome = async (req, res) =>{
+    const {id} = req.params;
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income) =>{
+            res.status(200).json({message: 'Income Deleted'})
+        })
+        .catch((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
+}
+
+
+
 
 
 
--- backend/routing/Router.js
+++ backend/routing/Router.js
@@ -1,23 +1,18 @@
 const Router = require('express')
 const router = new Router()
-//const controller = require('../controllers/Controller')
-const { addExpense,getExpense } = require('../controllers/Controller');
-const { addIncome, getIncomes } = require('../controllers/Controller');
+const { addExpense,getExpense, deleteExpense, deleteIncome, addIncome, getIncomes } = require('../controllers/Controller');
 
 router.get('/',(req,res) => {
     res.render('index');
 });
 
-//const router = require('express').Router();
 
 router.post('/add-income',addIncome)
     .get('/get-incomes', getIncomes)
     .post('/add-expense', addExpense)
     .get('/get-expenses', getExpense)
-   
-
-
-
+    .delete('/delete-income/:id', deleteIncome)
+    .delete('/delete-expense/:id', deleteExpense)
 
 
 module.exports = router
