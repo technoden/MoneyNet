81fcb3fa130ccb416774a459adb82cab7656dd4b
Daniil Voishel <127515557+technoden@users.noreply.github.com>
2023-04-26 17:05:21 +0200

    Update home.js

--- backend/views/js/home.js
+++ backend/views/js/home.js
@@ -4,7 +4,7 @@ window.onload = () => {
     if(!sessionStorage.name){
         location.href = '/login';
     } else{
-        greeting.innerHTML = `hello ${sessionStorage.name}`;
+        greeting.innerHTML = `${sessionStorage.name}`;
     }
 }
 
